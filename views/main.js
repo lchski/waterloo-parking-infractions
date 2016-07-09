aef4bc4d4e645dbc0f06334e317cb583eb6ec382
Lucas Cherkewski <lucas@ecustom.ca>
2016-07-09 14:35:03 -0400

    formatting

--- views/main.js
+++ views/main.js
@@ -3,16 +3,16 @@ const html = require('choo/html')
 module.exports = (state, prev, send) => {
     return html`
     <div onload=${() => send('getStreets')}>
-      <h1>Streets</h1>
-      <input type="text" placeholder="Search" oninput=${(e) => {
-        send('filterStreets', {query: e.target.value})
-      }}>
-      <form onsubmit=${onClear}>
-        <input type="submit" value="Clear">
-      </form>
-      <ul>
-        ${state.filteredStreets.map((street) => html`<li>${street}</li>`)}
-      </ul>
+        <h1>Streets</h1>
+        <input type="text" placeholder="Search" oninput=${(e) => {
+            send('filterStreets', {query: e.target.value})
+        }}>
+        <form onsubmit=${onClear}>
+            <input type="submit" value="Clear">
+        </form>
+        <ul>
+            ${state.filteredStreets.map((street) => html`<li>${street}</li>`)}
+        </ul>
     </div>`
 
     function onClear(e) {
