373db53bd55edb14c2dc6e93fa5156b15319fcaf
Lucas Cherkewski <lucas@ecustom.ca>
2016-07-10 11:34:32 -0400

    use csv to format the data

--- views/d3/time.js
+++ views/d3/time.js
@@ -42,50 +42,45 @@ module.exports = (state, prev, send) => {
             .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(x));
 
-        d3.json("./aggregate_infraction_time.json", function(error, data) {
+        d3.csv("./aggregate_infraction_time.csv", type, function(error, data) {
             if (error) throw error;
 
-            // let parsedData = d3.map(data, (d) => {
-            //     d.rate = +d.value / 365 * 60;
-            //     d.time = parseTime(d.key);
-            //     d.time.setUTCHours((d.time.getUTCHours() + 24 - 7) % 24);
-            //
-            //     return d;
-            // });
+            y.domain([0, d3.max(data, function(d) { return d.rate; })]);
 
-            y.domain([0, d3.max(data, function(d) { return d.key; })]);
+            svg.append("g")
+                .attr("class", "axis axis--x")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x)
+                    .tickFormat(d3.utcFormat("%I %p")));
 
-            // svg.append("g")
-            //     .attr("class", "axis axis--x")
-            //     .attr("transform", "translate(0," + height + ")")
-            //     .call(d3.svg.axis()
-            //         .scale(x)
-            //         .orient("bottom")
-            //         .tickFormat(d3.utcFormat("%I %p")));
+            svg.append("g")
+                .attr("class", "dots")
+                .selectAll("path")
+                .data(data)
+                .enter().append("path")
+                .attr("transform", function(d) { return "translate(" + x(d.time) + "," + y(d.rate) + ")"; })
+                .attr("d", d3.symbol()
+                    .size(40));
 
-            // svg.append("g")
-            //     .attr("class", "dots")
-            //     .selectAll("path")
-            //     .data(parsedData)
-            //     .enter().append("path")
-            //     .attr("transform", function(d) { return "translate(" + x(d.time) + "," + y(d.rate) + ")"; })
-            //     .attr("d", d3.svg.symbol()
-            //         .size(40));
+            var tick = svg.append("g")
+                .attr("class", "axis axis--y")
+                .call(d3.axisLeft(y)
+                    .tickSize(-width))
+                .select(".tick:last-of-type");
 
-            // var tick = svg.append("g")
-            //     .attr("class", "axis axis--y")
-            //     .call(d3.svg.axis()
-            //         .scale(y)
-            //         .tickSize(-width)
-            //         .orient("left"))
-            //     .select(".tick:last-of-type");
+            var title = tick.append("text")
+                .attr("dy", ".32em")
+                .text("tweets per hour");
 
-            // var title = tick.append("text")
-            //     .attr("dy", ".32em")
-            //     .text("tweets per hour");
-
-            // tick.select("line")
-            //     .attr("x1", title.node().getBBox().width + 6);
+            tick.select("line")
+                .attr("x1", title.node().getBBox().width + 6);
         });
+
+        function type(d) {
+            d.rate = +d.count;
+            d.time = parseTime(d.time);
+            d.time.setUTCHours((d.time.getUTCHours() + 24 - 7) % 24);
+            return d;
+        }
     }
 }
